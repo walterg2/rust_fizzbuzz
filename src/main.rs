dad352e56fa5d8b8a148b5f595027821c76a4db2
George Walters <walterg2@gmail.com>
2021-03-05 08:35:04 -0500

    Adding tests to return fizz when it's a multiple of 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn fizzbuzz(num: i8) -> String {
-    if num == 3 {
+    if num % 3 == 0 {
         return "fizz".to_string();
     }
     return num.to_string();
@@ -10,16 +10,23 @@ mod tests {
     use crate::fizzbuzz;
 
     #[test]
-    fn test_fizzbuzz_returns_given_number() {
+    fn test_it_returns_given_number() {
         let result = fizzbuzz(1);
 
         assert_eq!(result, "1");
     }
 
     #[test]
-    fn test_fizzbuzz_returns_fizz_when_given_three() {
+    fn test_it_returns_fizz_when_given_three() {
         let result = fizzbuzz(3);
 
         assert_eq!(result, "fizz");
+    }
+
+    #[test]
+    fn test_it_returns_fizz_when_given_a_multiple_of_three() {
+        assert_eq!(fizzbuzz(6), "fizz");
+        assert_eq!(fizzbuzz(9), "fizz");
+        assert_eq!(fizzbuzz(12), "fizz");
     }
 }
